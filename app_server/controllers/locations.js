0a6cc134f1d3f1205b08f8118d024e8049774404
DNwin <ndennis@gmail.com>
2015-12-13 18:58:18 -0800

    moved data from view templates to controllers

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -5,15 +5,94 @@
 
 /* GET 'home' page */
 module.exports.homelist = function(req, res) {
-    res.render('locations-list', { title: 'Home' });
+    res.render('locations-list', {
+        title: 'Home',
+        pageHeader: {
+            title: 'LocateR',
+            strapline: 'Find places to work with wifi near you.'
+        },
+        locations: [{
+            name: 'Starcups',
+            address: '123 Star Street, Tustin',
+            rating: 3,
+            facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+            distance: '100m'
+        }, {
+            name: 'LU Electric',
+            address: '123 Mitchell, Irvine',
+            rating: 4,
+            facilities: ['Food', 'Premium wifi'],
+            distance: '300m'
+        }, {
+            name: 'Dnwin',
+            address: '123 Nice St, Irvine',
+            rating: 5,
+            facilities: ['Food', 'Premium wifi, Beds'],
+            distance: '1m'
+        }
+
+
+
+
+
+        ]});
 };
 
 /* GET 'Location info' page */
 module.exports.locationInfo = function(req, res) {
-    res.render('location-info', { title: 'Location Info'});
+    res.render('location-info', {
+        title: 'Starcups',
+        pageHeader: {
+            title: 'Starcups'
+        },
+        sidebar: {
+            context: 'is on LocateR because it has accessible wifi and space to sit down with your laptop and get some work done.',
+            callToAction: 'If you\'ve been and you like it - or if you don\'t - please leave a review to help other people just like you.'
+        },
+        location: {
+            name: 'Starcups',
+            address: '125 High Street, Reading, RG6 1PS',
+            rating: 3,
+            facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+            coords: {
+                lat: 51.455041,
+                lng: -0.9690884
+            },
+            openingTimes: [{
+                days: 'Monday - Friday',
+                opening: '7:00am',
+                closing: '7:00pm',
+                closed: false
+            }, {
+                days: 'Saturday',
+                opening: '8:00am',
+                closing: '5:00pm',
+                closed: false
+            }, {
+                days: 'Sunday',
+                closed: true
+            }],
+            reviews: [{
+                author: 'Simon Holmes',
+                rating: 5,
+                timestamp: '16 July 2013',
+                reviewText: 'What a great place. I can\'t say enough good things about it.'
+            }, {
+                author: 'Charlie Chaplin',
+                rating: 3,
+                timestamp: '16 June 2013',
+                reviewText: 'It was okay. Coffee wasn\'t great, but the wifi was fast.'
+            }]
+        }
+    });
 };
 
 /* GET 'Add review' page */
 module.exports.addReview = function(req, res) {
-    res.render('location-review-form', { title: 'Add review'});
+    res.render('location-review-form', {
+        title: 'Review Starcups on LocateR',
+        pageHeader: {
+            title: 'Review Starcups'
+        }
+    });
 };
--- app_server/controllers/others.js
+++ app_server/controllers/others.js
@@ -3,5 +3,7 @@
  */
 
 module.exports.about = function(req, res) {
-    res.render('generic-text', {title: 'About'});
+    res.render('generic-text', {
+        title: 'About',
+        content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'});
 };
